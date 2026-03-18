f6618e22921c1e990a144a3660f06f57bc4fd089
sonicstiker-art <sonicstiker@gmail.com>
2026-03-18 19:06:06 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import os
 import discord
-import asyncio
+import asyncio  # ✅ เพิ่มตรงนี้
 from discord.ext import commands
 
 from myserver import server_on
 
 intents = discord.Intents.default()
-intents.members = True  # สำคัญมาก
+intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -21,9 +21,8 @@ async def ping(ctx):
 
 @bot.event
 async def on_member_join(member):
-    
     await asyncio.sleep(1)
-    
+
     channel = member.guild.get_channel(1483772251378683954)
 
     if channel:
@@ -32,29 +31,25 @@ async def on_member_join(member):
             description=f"""
 ยินดีต้อนรับ {member.mention}
 welcome to ♪.  ALTAFAIR  ⟢₊ ˚. *!*
-⠀⯎⠀⠀*!*⠀{member.mention} ⠀﹒⠀***
- ⠀<:RV:1447295941248942282><:02role:1447296966995218452>
- <:churchofgod:1447295755672096819>⠀﹐⠀⠀➷⠀<:emoji_231:1447295809488945195>
-︵***⠀
- ⠀welcome   {member.mention}   ⠀﹐⠀⠀➷⠀
-the person {member.guild.member_count}⠀‿⠀wlc *! *** ˚ ₊
+
+👥 สมาชิก: {member.guild.member_count}
 """,
             color=discord.Color.red()
         )
 
-        # รูปแบนเนอร์ (ใส่ลิงก์รูปเอง)
-        embed.set_image(url="https://cdn.discordapp.com/attachments/1483407902323773440/1483791975911133274/ChatGPT_Image_18_.._2569_00_28_18.png?ex=69bbe076&is=69ba8ef6&hm=663b8c85c3be49a6d1c9d57c95fc1461d4807bc1338db4a8929d0a5781b981d2&")
+        embed.set_image(
+            url="https://cdn.discordapp.com/attachments/1483407902323773440/1483791975911133274/ChatGPT_Image_18_.._2569_00_28_18.png"
+        )
 
         embed.set_author(
-        name=member.name,
-        icon_url=member.avatar.url if member.avatar else member.default_avatar.url
-)
+            name=member.name,
+            icon_url=member.avatar.url if member.avatar else member.default_avatar.url
+        )
 
-        # ✅ ส่ง embed
         await channel.send(
-    content=member.mention,
-    embed=embed
-)
+            content=member.mention,
+            embed=embed
+        )
 
 server_on()
 
